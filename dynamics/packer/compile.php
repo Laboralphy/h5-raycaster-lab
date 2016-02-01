0585175083923fceed0f953eef2713bbfb8130b6
Laboralphy <raphael.marandet@gmail.com>
2016-02-01 01:32:37 +0100

    tutorial1

--- dynamics/packer/compile.php
+++ dynamics/packer/compile.php
@@ -3,7 +3,7 @@ require_once('ScriptLoader.php');
 require_once('JavascriptPacker.php');
 require_once('helper.php');
 
-if (preg_match('/[a-z0-9]+/i', isset($_GET['f']))) {
+if (preg_match('/[-_a-z0-9]+/i', isset($_GET['f']))) {
 	if (isset($_GET['f'])) {
 		$s = compileScript('../../sources/' . $_GET['f'] . '/load');
 		header('Content-type: text/javascript');
