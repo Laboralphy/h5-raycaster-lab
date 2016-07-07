27a5aae5a80ca3a5068a659292aea5cb516dbca2
Laboralphy <raphael.marandet@gmail.com>
2016-07-07 14:22:35 +0200

    changed rcwe font and icon

--- website/index.php
+++ website/index.php
@@ -6,31 +6,22 @@ include 'mainmenu.php';
 
 <div class="row">
 	<div class="col-lg-12">
+		<div class="card">
 		<div class="row">
 			<div class="col-lg-4 col-lg-offset-2 col-sm-6 col-sm-offset-0 col-xs-12" style="vertical-align: middle; height: 5em;">
-				<div class="card">
-					<h3 style="text-align: justify">Welcome to the Raycaster Javascript Framework website.</h3>
-				</div>
+				<h3 style="text-align:center">Welcome to the Raycaster Javascript Framework website.</h3>
 			</div>
 			<div class="col-lg-4 col-sm-6 col-xs-12">
-				<div class="card">
-					<b>What I do here :</b>
-					<ul>
-						<li>Game development</li>
-						<li>HTML 5 games dev</li>
-						<li>Raycasting games dev</li>
-					</ul>
-				</div>
+				<b>What I usualy do here :</b>
+				<ul>
+					<li>Game development</li>
+					<li>HTML 5 games dev</li>
+					<li>Raycasting games dev</li>
+				</ul>
 			</div>
 		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="col-lg-12">
-		<div class="card">
-			<p>Then you might want to pay a visit here. Because that's what I do : developing HTML 5 raycasting games (among other things).</p>
 		</div>
-	<div>
+	</div>
 </div>
 <?php
 include 'footer.php';
