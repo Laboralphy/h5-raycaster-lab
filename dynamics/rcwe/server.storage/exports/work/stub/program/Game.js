8f5d94055f11590b6a436af6d847ac2eaff28134
Laboralphy <raphael.marandet@gmail.com>
2016-10-13 17:39:29 +0200

    fixed rcwe build systeme

--- dynamics/rcwe/server.storage/exports/work/stub/program/Game.js
+++ dynamics/rcwe/server.storage/exports/work/stub/program/Game.js
@@ -1,5 +1,5 @@
 /* globals O2, O876_Raycaster */
-O2.extendClass('$GAMENAME.Game', O876_Raycaster.GameAbstract, {
+O2.extendClass('STUB.Game', O876_Raycaster.GameAbstract, {
 	init: function() {
 		this.on('leveldata', function(wd) {
 			wd.data = LEVEL_DATA[Object.keys(LEVEL_DATA)[0]];
